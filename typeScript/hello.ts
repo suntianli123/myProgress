148652e079eeb8d8637350fe43ccacd034e00a5a
suntianli <369034946@163.com>
2021-07-22 16:35:33 +0800

    react

---
+++ typeScript/hello.ts
@@ -0,0 +1,16 @@
+console.log('ts112233')
+
+// 一：基本类型
+let a:number;
+a = 10;
+
+let b:string;
+b= 'string';
+
+let c: boolean = false;
+
+let d: number[] = [1, 2, 3];
+let e: Array<number> = [1, 2, 3];
+
+// 元组 Tuple
+let f: [string, number] = ['hello', 10];
